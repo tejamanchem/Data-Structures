d5b4fac961b49622207276f00725f1f85c2100a9
teja <tejamanchem144@gmail.com>
2022-06-05 17:57:01 +0530

    middle element in list

---
+++ findingmiddleElement.py
@@ -0,0 +1,23 @@
+from tkinter import E
+
+
+list1 =[]
+print('enter how many elements do you want to add to list:')
+for i in  range(0,int(input())):
+    list1.append(input())
+
+print('how many mid elements you want:')
+k = int(input())
+
+startindex= (len(list1)//2)-(k//2)
+endindex = (len(list1)//2)+(k//2)
+print(startindex)
+print(endindex)
+
+result =[]
+
+for j in range(len(list1)):
+    if j>=startindex and j<endindex:
+        result.append(list1[j])
+
+print('extracted mid elements are',str(result))
