5a089b786306381b33a3d3767c5f2f42f145010f
teja <tejamanchem144@gmail.com>
2022-06-05 18:21:43 +0530

    middle element in linked list

---
+++ middleElementInLinkedList.py
@@ -0,0 +1,46 @@
+
+# Python 3 program to find the middle of a
+# given linked list
+
+class Node:
+	def __init__(self, value):
+		self.data = value
+		self.next = None
+	
+class LinkedList:
+
+	def __init__(self):
+		self.head = None
+
+	# create Node and and make linked list
+	def push(self, new_data):
+		new_node = Node(new_data)
+		new_node.next = self.head
+		self.head = new_node
+		
+	def printMiddle(self):
+		temp = self.head
+		count = 0
+		
+		while self.head:
+
+			# only update when count is odd
+			if (count & 1):
+				temp = temp.next
+			self.head = self.head.next
+
+			# increment count in each iteration
+			count += 1
+		
+		print(temp.data)	
+		
+# Driver code
+llist = LinkedList()
+llist.push(1)
+llist.push(2)
+llist.push(3)
+
+llist.printMiddle()
+# code has been contributed by - Yogesh Joshi
+
+           
