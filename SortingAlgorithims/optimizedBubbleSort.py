9735c3af1648c2c6816a5665ef0264681ef6d9e3
teja <tejamanchem144@gmail.com>
2022-06-09 17:44:46 +0530

    bubble sort

---
+++ SortingAlgorithims/optimizedBubbleSort.py
@@ -0,0 +1,33 @@
+def bubbleSort(arr):
+
+    for i in range(len(arr)):
+
+        swap = False
+
+        for j in range(0,len(arr)-i-1):
+
+            if arr[j] > arr[j+1]:
+
+                temp = arr[j]
+
+                arr[j]= arr[j+1]
+
+                arr[j+1] = temp
+
+            if not swap:
+                break
+
+
+noOfElements = int(input())
+
+l1 =[]
+
+for k in range(0,noOfElements):
+    l1.append(int(input()))
+
+bubbleSort(l1)
+
+print(l1)
+
+
+
